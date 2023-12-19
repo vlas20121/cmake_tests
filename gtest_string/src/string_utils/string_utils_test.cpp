9de4cc469a832113e3198ca745b09deebcd916b4
vlas20121 <vlas20121@gmail.com>
2023-12-19 17:51:00 +0300

    Fixed _test.* globbing rules

--- gtest_string/src/string_utils/string_utils_test.cpp
+++ gtest_string/src/string_utils/string_utils_test.cpp
@@ -1,3 +1,4 @@
+#ifdef TESTS
 #include <gtest/gtest.h>
 #include <string_utils/string_utils.h>
 #include <string>
@@ -17,12 +18,4 @@ TEST(isStartedWith, isStartedWith_bad)
 	ASSERT_TRUE(!isStartedWith_bad(s.data(), s.data()+s.size(), "abce"));
     ASSERT_TRUE(!isStartedWith(s.data(), s.data() + s.size(), "abcdefg12345"));
 }
-
-
-/*
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-*/
+#endif
