1cc1f01fad8a4c970a2f4a42381ad9cd61db1df5
vlas20121 <vlas20121@gmail.com>
2024-02-09 12:34:49 +0300

    +=check_pvs_commenting

---
+++ check_pvs_commenting/main.cpp
@@ -0,0 +1,14 @@
+//lakely PVS-Studio bug
+
+enum Result { Result1 = 1, Result2 = 2 };
+
+int main()
+{
+    Result result = Result1;
+    switch (result)
+    {
+    case/* */Result1:   //V729 Function body contains the 'caseResult1' label that is not used by any 'goto' statements. D:\GitHub\TestGroup\cmake_tests\check_pvs_commenting\main.cpp 11
+        break;
+    }
+}
+
