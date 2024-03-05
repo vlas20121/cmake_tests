037dce3249cb93cfa2d8a9d57e0d5a280ab428fa
vlas20121 <vlas20121@gmail.com>
2024-03-05 19:18:33 +0300

    updated Geom

--- Geom/main.cpp
+++ Geom/main.cpp
@@ -5,6 +5,9 @@
 #include <limits>
 #include <cassert>
 
+/// <summary>
+/// basec numerical
+/// </summary>
 namespace BaseNumerical {
     template<class NUMBER>
     bool equ_approx(NUMBER v0, NUMBER v1)
@@ -14,6 +17,9 @@ namespace BaseNumerical {
     }
 }
 
+/// <summary>
+/// generic geometry
+/// </summary>
 namespace BaseGeometry {
     using namespace BaseNumerical;
 
@@ -71,8 +77,23 @@ namespace BaseGeometry {
     {
         return std::sqrt(abs2(v));
     }
+    template<class VEC,class STREAM>
+    void to_stream(STREAM& stream, const VEC& v)
+    {
+        stream << "vec" << v.size() << "{";
+        for (int i = 0; i < v.size(); i++)
+        {
+            stream << v[i];
+            if(i<v.size()-1)
+                stream << ",";
+        }
+        stream << "}";
+    }
 };
 
+/// <summary>
+/// some 2d/3d geometry
+/// </summary>
 namespace Geometry{
     using namespace BaseGeometry;
     using vec2 = vec<double, 2>;
@@ -93,37 +114,55 @@ namespace Geometry{
         return double();
 	}
 
+    /// <summary>
+    /// circle parameters
+    /// </summary>
     struct circle
     {
-        vec2 c;
-        double r;
+        vec2 center;
+        double radius;
     };
-    vec2 Rotate(const vec2& p, double _sin, double _cos)
+
+    /// <summary>
+    /// Rotate vector by angle 
+    /// </summary>
+    /// <param name="p">vector for rotae</param>
+    /// <param name="rot">rotate direction; rot=vec2{sin(angle),cos(angle)}</param>
+    /// <returns>rotated vector</returns>
+    vec2 Rotate(const vec2& p, const vec2& rot)
     {
         return vec2{
-            p[0] * _cos - p[1] * _sin,
-            p[0] * _sin - p[1] * _cos
+            p[0] * rot[1] - p[1] * rot[0],
+            p[0] * rot[0] + p[1] * rot[1]
         };
     }
+    /// <summary>
+    /// return tangent point's from point to circle
+    /// </summary>
+    /// <param name="circ">circe</param>
+    /// <param name="p">from point</param>
+    /// <param name="second">1st or 2st tangent point</param>
+    /// <returns>tangent point</returns>
     vec2 circle_tangent_point(const circle& circ, const vec2& p, bool second=false)
     {
-        assert(circ.r > 0);
-        vec2 l = circ.c - p;
+        assert(circ.radius > 0);
+        vec2 l = circ.center - p;
         double L = abs(l);
-        assert(circ.r<=L);
-        double _sin = circ.r / L;
+        assert(circ.radius<=L);
+        double _sin = circ.radius / L;
         double _cos = std::sqrt(1 - _sin * _sin);
         if (second)
             _sin = -_sin;
-        vec2 _l = Rotate(l, _sin, _cos);
-        vec2 _t = _l*_cos;
-        vec2 t = p+_t;
+        vec2 t = p + Rotate(l, vec2{ _sin, _cos }) * _cos;
         return t;
     }
 };
 
 // immutable:
 
+/// <summary>
+/// testing geometry immutability
+/// </summary>
 void immutable()
 {
     using namespace Geometry;
@@ -134,14 +173,22 @@ void immutable()
 
 using namespace Geometry;
 
+void circle_tangent_point_check(const circle& circ, const vec2& p, const vec2& t)
+{
+    double _dot = dot(t - circ.center, t - p);
+    assert(equ_approx(_dot, 0.));
+}
+
 void circle_tangent_point_test(const circle& circ, const vec2& p)
 {
     vec2 t0 = circle_tangent_point(circ, p);
     vec2 t1 = circle_tangent_point(circ, p, true);
-    auto dot0 = dot(t0 - circ.c, t0 - p);
-    auto dot1 = dot(t1 - circ.c, t1 - p);
-    assert(equ_approx(dot0, 0.));
-    assert(equ_approx(dot1, 0.));
+
+    circle_tangent_point_check(circ, p, t0);
+    circle_tangent_point_check(circ, p, t1);
+
+    std::cout << "circle_tangent_point_test:";
+    to_stream(std::cout, t0); std::cout << ","; to_stream(std::cout, t1); std::cout << "\n";
 }
 
 void circle_tangent_point_test()
@@ -154,6 +201,10 @@ void circle_tangent_point_test()
 
 void main()
 {
+    [[maybe_unused]] auto v2def = vec2();
+    [[maybe_unused]] vec2 vt_c;
+    [[maybe_unused]] vec2 vt_c1({1., 2.});
+
     auto v1 = vec3{ 1,2,3 } + vec3{ 3,2,1 };
     auto v2 = vec3{ 1,2,3 } *2.;
 
@@ -168,6 +219,5 @@ void main()
     auto bls0 = Distance(ls, pl[0]);
     auto b0 = Distance(pl, pl[0]);
     auto b1 = Distance(pl, vec3{ 0.5,0.5,0.5 });
-    auto v2def = vec2();
     circle_tangent_point_test();
 }
